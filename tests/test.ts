a99bc3e76cddb9de8f08cf7e2875b621a6e7a15f
Vanessa <vybarnard@gmail.com>
2023-01-31 11:21:07 -0800

    remove db

--- tests/test.ts
+++ tests/test.ts
@@ -1,8 +1,8 @@
 import net from 'net'
 
 const netSocket = new net.Socket();
-netSocket.connect(18018, '0.0.0.0');
-//netSocket.connect(18018, '45.77.189.227'); //(not local)
+//netSocket.connect(18018, '0.0.0.0');
+netSocket.connect(18018, '45.77.189.227'); //(not local)
 netSocket.write('{ "type": "hello","version": "0.9.0","agent": "Marabu-Core Client 0.9"}\n');
 netSocket.on('data',(data: Buffer) => console.log(data.toString()));
 
