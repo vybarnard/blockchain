af7c19537a316d237038f7a358abbf6c349cd01b
vybarnard <68301134+vybarnard@users.noreply.github.com>
2023-03-01 16:11:07 -0800

    Delete src directory

--- src/crypto/hash.ts
+++
@@ -1,9 +0,0 @@
-import blake2 from 'blake2'
-
-export function hash(str: string) {
-  const hash = blake2.createHash('blake2s')
-  hash.update(Buffer.from(str))
-  const hashHex = hash.digest('hex')
-
-  return hashHex
-}
